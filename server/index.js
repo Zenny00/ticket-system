7503503b5ae473ab0cba9f702b1658c68d7c47c8
Zenny00 <joshuacomfort@gmail.com>
2025-09-17 20:54:57 -0400

    Setup nginx

--- server/index.js
+++ server/index.js
@@ -1,15 +1,36 @@
-const express = require("express");
+import pg from 'pg';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from "cors";
+
+const { Client } = pg;
 const app = express();
-const cors = require("cors");
+
+/**
+ * Function for establishing a connection with the database
+ *
+ */
+async function establishDatabaseConnection(user, host, database, password, port) {
+  const client = new Client({
+    user: user,
+    host: host,
+    database: database,
+    password: password,
+    port: port,
+  });
+  await client.connect();
+}
 
 // Setup cross origin resource sharing to allow requests to be made from our client to the server
 app.use(cors());
 
-app.listen(8080, () => {
-  console.log("Server is listening on port 8080");
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
 });
 
 // Homepage route
 app.get("/", (req, res) => {
   res.send("Hello from the server!");
 });
+
+// establishDatabaseConnection("postgres", "db", "postgres", "password", 5432);
