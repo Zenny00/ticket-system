ad0ccbb9c9244a95ee04765a390081fb2b5b4285
Zenny00 <joshuacomfort@gmail.com>
2025-09-18 20:04:36 -0400

    Fix vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+    allowedHosts: ['front-end', 'back-end']
     //port: 5173, When not running with docker compose, this is the port which will be used in docker
   }
 })
