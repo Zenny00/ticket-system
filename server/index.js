71b68aa9965a04aa851c9da610e9f7ef4076253c
Zenny00 <joshuacomfort@gmail.com>
2025-11-05 21:12:24 -0500

    Create connection pool and return data

--- server/index.js
+++ server/index.js
@@ -3,26 +3,31 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from "cors";
 
-const { Client } = pg;
+const { Pool } = pg;
 const app = express();
 
 /**
- * Function for establishing a connection with the database
+ * Function for establishing a connection pool with the database
  *
  */
-async function establishDatabaseConnection(user, host, database, password, port) {
-  const client = new Client({
+async function establishDatabaseConnectionPool(user, password, host, database, port) {
+  const pool = new Pool({
     user: user,
+    password: password,
     host: host,
     database: database,
-    password: password,
     port: port,
   });
-  await client.connect();
+
+  return pool;
 }
 
 // Setup cross origin resource sharing to allow requests to be made from our client to the server
 app.use(cors());
+
+let pool = await establishDatabaseConnectionPool("ticketmanager", "placeholder", "db", "ticketsystem", 5432);
+let connection = await pool.connect();
+let query = await connection.query('SELECT * FROM users');
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
@@ -30,7 +35,11 @@ app.listen(3000, () => {
 
 // Homepage route
 app.get("/", (req, res) => {
+  
+  console.log(query);
+
+  console.log("Connection established with DB!");
   res.send("Hello from the server!!");
+
 });
 
-// establishDatabaseConnection("postgres", "db", "postgres", "password", 5432);
